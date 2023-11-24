539f8286f79c3b059bbc45fcda1fe65705f2e7af
shibatetsuya <ogulewe.e.f@gmail.com>
2023-11-23 21:10:39 -0500

    qwerty

--- SmartHome_IoT/src/GUI.java
+++ SmartHome_IoT/src/GUI.java
@@ -30,11 +30,13 @@ public class GUI extends Application {
 	GUIController controller = loader.getController();
 	loader.setController(controller);
 
-	SmartHomeClient client = new SmartHomeClient("172.17.11.45", 2090, controller);
+	SmartHomeClient client = new SmartHomeClient("172.17.11.", 2010, controller);
 
-	client.connectToServer();
+   client.connectToServer();
 
-	controller.setClient(client);
+   controller.setClient(client);
+
+
 
 //
 
@@ -54,6 +56,7 @@ public class GUI extends Application {
 
     public static void main(String[] args) {
 
+    	
 	// launch contact app object and start method
 	launch(args);
 
