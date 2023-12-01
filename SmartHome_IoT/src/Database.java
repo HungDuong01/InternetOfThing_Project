c28c1e717f7c0e0018fb04b4ccc75435cdebb2fb
Tommy <91548470+HungDuong01@users.noreply.github.com>
2023-11-30 20:31:07 -0500

    .

--- SmartHome_IoT/src/Database.java
+++ SmartHome_IoT/src/Database.java
@@ -1,41 +1,80 @@
-
-/* ------------------------------------------------------------------------------------
- * IoTController.java
-
- * 
- * Copyright (c) 2023 Venos Tech. All rights reserved
- * 
- * Related Documents: 
- *    Specification Document 
- *    Design Document
- * 
- * File created by Duong Chan Hung on 11/25/2023
- * 
- * Associated file: IoTServer.java (one to one) 
- *                  SmartDevice.java (many to one) 
- * ------------------------------------------------------------------------------------
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Database {
+    private static final String URL = "jdbc:mysql://localhost:3306/SH_Account";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
-    private static final String URL = "jdbc:mysql://localhost:3306/userDB";
-    private static final String USER = "yourUsername";
-    private static final String PASSWORD = "yourPassword";
+    public static Connection getConnection() throws SQLException {
+	return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
-    public static Connection getConnection() {
-	try {
-	    return DriverManager.getConnection(URL, USER, PASSWORD);
+    public boolean signUpUser(String firstName, String lastName, String email, String password) {
+	String query = "INSERT INTO User (FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?)";
+
+	try (Connection conn = Database.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+	    pstmt.setString(1, firstName);
+	    pstmt.setString(2, lastName);
+	    pstmt.setString(3, email);
+	    pstmt.setString(4, password);
+
+	    // Check if user already exists
+	    if (accountExist(email)) {
+		System.out.println("User already exists.");
+		return false;
+	    }
+
+	    int result = pstmt.executeUpdate();
+	    return result > 0;
+
 	} catch (SQLException e) {
-	    throw new RuntimeException("Error connecting to the database", e);
+	    System.out.println("SQLException: " + e.getMessage());
+	    return false;
 	}
     }
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
+    public boolean signUpAdmin(String firstName, String lastName, String email, String password) {
+	String query = "INSERT INTO User (FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?)";
 
+	try (Connection conn = Database.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+	    pstmt.setString(1, firstName);
+	    pstmt.setString(2, lastName);
+	    pstmt.setString(3, email);
+	    pstmt.setString(4, password);
+
+	    // Check if user already exists
+	    if (accountExist(email)) {
+		System.out.println("User already exists.");
+		return false;
+	    }
+
+	    int result = pstmt.executeUpdate();
+	    return result > 0;
+
+	} catch (SQLException e) {
+	    System.out.println("SQLException: " + e.getMessage());
+	    return false;
+	}
     }
 
+    private boolean accountExist(String email) throws SQLException {
+	String query = "SELECT COUNT(*) FROM users WHERE username = ?";
+
+	try (Connection conn = Database.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+	    pstmt.setString(1, email);
+	    try (ResultSet rs = pstmt.executeQuery()) {
+		if (rs.next()) {
+		    return rs.getInt(1) > 0;
+		}
+	    }
+	}
+	return false;
+    }
 }
--- SmartHome_IoT/src/User.java
+++ SmartHome_IoT/src/User.java
@@ -15,8 +15,8 @@
  */
 public class User extends Account {
 
-    public User(String userName, String password) {
-	super(userName, password);
+    public User(String email, String password) {
+	super(email, password);
     }
 
 }
