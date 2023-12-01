9854b271bc8cdd6ac9e129687f268980fe64b479
Tommy <91548470+HungDuong01@users.noreply.github.com>
2023-11-30 21:11:27 -0500

    .

--- SmartHome_IoT/src/Database.java
+++ SmartHome_IoT/src/Database.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/SH_Account";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "Hungyeuem123";
 
     public static Connection getConnection() throws SQLException {
 	return DriverManager.getConnection(URL, USER, PASSWORD);
